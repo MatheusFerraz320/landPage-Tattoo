462de40a0a638b2544d22b5cafa84adecd953f3b
Matheus Ferraz <matheusaugustosabinoferraz@outlook.com>
2025-11-23 20:04:41 -0300

    Correção de nome em uppercase

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import Navbar from "./components/Navbar";
+import NavBar from "./components/Navbar";
 import Home from "./components/Home";
 import About from "./components/About";
 import Portfolio from "./components/Portfolio";
